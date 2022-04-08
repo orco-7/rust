67d219415ecb2c28f43f1095643668def0e5a323
Simone Martelli <simone.martelli@pm.me>
2022-04-08 16:36:23 +0200

    chapter 6

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,13 @@
+struct Color(i32, i32, i32);   // a function that takes Color as argument can't
+struct Point(i32, i32, i32);   // take a Point even though they have the same structure
+struct AlwaysEqual;            // implement a type without any data
 fn main() {
-    println!("Hello, world!");
+    let black = Color(0, 0, 0);
+
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+
+    println!("{}", subject);
+
 }
