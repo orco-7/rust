55fa794bcade4c08b71bf4715d01642265a96247
Simone Martelli <simone.martelli@pm.me>
2022-04-27 12:15:09 +0200

    generics

---
+++ gen/target/debug/deps/gen-c1ce343721e50414.d
@@ -0,0 +1,5 @@
+/home/simone/rust/gen/target/debug/deps/gen-c1ce343721e50414: src/main.rs
+
+/home/simone/rust/gen/target/debug/deps/gen-c1ce343721e50414.d: src/main.rs
+
+src/main.rs:
---
+++ gen/target/debug/gen.d
@@ -0,0 +1,1 @@
+/home/simone/rust/gen/target/debug/gen: /home/simone/rust/gen/src/main.rs
